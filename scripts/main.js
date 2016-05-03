0ac77bb8e1a24d2efc5383e81e786be2c888edb0
pcroce <pscroce@gmail.com>
2016-05-03 16:36:34 -0400

    Add bigfoot.js for footnotes and add padding to the tops of main sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,23 @@
 $(function() {
 
-  // enables background on navbar collapse
-  $('.navbar-toggle').click(function(){
-    $('.navbar-default').toggleClass('collapse-active');
-  });
 
-  // on .page-content click, if .collapse-active exists on the page,
-  // collapse the navbar (which is the mobile one) and toggleClass
-  // .collapse-active (which has the white transparent background style)
-  $(".page-content").click(function(){
-    if ($(".collapse-active")[0]) {
-      $('.navbar-collapse').collapse('hide');
-      $('.navbar-default').toggleClass('collapse-active');
-    }
-  });
+
+// enables background on navbar collapse
+$('.navbar-toggle').click(function(){
+  $('.navbar-default').toggleClass('collapse-active');
+});
+
+// on .page-content click, if .collapse-active exists on the page,
+// collapse the navbar (which is the mobile one) and toggleClass
+// .collapse-active (which has the white transparent background style)
+$(".page-content").click(function(){
+  if ($(".collapse-active")[0]) {
+    $('.navbar-collapse').collapse('hide');
+    $('.navbar-default').toggleClass('collapse-active');
+  }
+});
+
+// required for bigfoot
+$.bigfoot();
 
 });
